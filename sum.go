8b2a47f8f8c471f6876dd16143fd9b10f1831335
Alexandre Da Matta <alexandre@jadlog.com.br>
2022-04-21 19:18:38 -0300

    ci: check if quality gate is working on sonar cloud

--- sum.go
+++ sum.go
@@ -10,14 +10,14 @@ func sum(a, b int) int {
 	return a + b
 }
 
-func sub(a, b int) int {
-	return a - b
-}
+// func sub(a, b int) int {
+// 	return a - b
+// }
 
-func times(a, b int) int {
-	return a * b
-}
+// func times(a, b int) int {
+// 	return a * b
+// }
 
-func sumX(a, b int) int {
-	return a + b + a
-}
+// func sumX(a, b int) int {
+// 	return a + b + a
+// }
